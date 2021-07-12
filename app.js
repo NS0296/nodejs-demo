5082bc72ab33c10c2596177cce88763793e438a1
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-07-12 13:24:58 +0200

    Add register route and html file

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ const app = express();
 //import routers
 const adminRouter = require('./routes/admin').router;
 const registerRouter = require('./routes/register-customer');
+const homeRouter = require('./routes/home');
 
 //body parser
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/admin', adminRouter);
 app.use(registerRouter);
+app.use(homeRouter);
 
 app.listen(3000);
