366af357543e73899f4de1eee25257f0ad9879d9
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-07-19 15:10:36 +0200

    Delete 404.js and add its middleware func to app.js

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const adminRouter = require(path.join(rootDir, 'routes', 'admin.js'));
 const usersRouter = require(path.join(rootDir, 'routes', 'users.js'));
 const companiesRouter = require(path.join(rootDir, 'routes', 'companies.js'));
 const homeRouter = require(path.join(rootDir, 'routes', 'home.js'));
-const status404 = require(path.join(rootDir, 'routes', '404.js'));
 
 const app = express();
 
@@ -31,6 +30,10 @@ app.use(adminRouter);
 app.use(usersRouter);
 app.use(companiesRouter);
 app.use(homeRouter);
-app.use(status404);
+
+//catch all
+app.use('/', (req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3000);
