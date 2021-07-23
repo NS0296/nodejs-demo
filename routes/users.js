e05bc44b7cb5467da4f91428b08e8186c29cf02c
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-07-23 17:18:45 +0200

    Add register for MongoDB

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const rootDir = require('../util/path');
 
@@ -13,9 +14,31 @@ router.get('/register', (req, res, next) => {
     res.render('user-register.ejs');
 });
 
-router.post('/register', (req, res, next) => {
-    db.registerUser(Object.values(req.body));
+router.post('/register', async (req, res, next) => {
+    const { username, email, password, phone, address } = req.body;
     res.redirect('/');
+
+    const hashPassword = await bcrypt.hash(password, 4);
+
+    //create new userModel
+    const newUser = new userModel({
+        username: username,
+        email: email,
+        password: hashPassword,
+        phone: parseInt(phone) || null,
+        address: address || null,
+    });
+
+    try {
+        const isUserExist = await userModel.exists({ email: email });
+        if (isUserExist) {
+            throw new Error('Email already exists for another user');
+        }
+
+        await newUser.save(); //insert the model inctance(aka document) into the db
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 //login
