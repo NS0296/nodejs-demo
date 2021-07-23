f211b4e3a42468f22124341cde0a5cdaccc646b8
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-07-23 14:53:25 +0200

    Connect to MongoDB, Add session middleware'

--- app.js
+++ app.js
@@ -2,23 +2,55 @@
 const path = require('path');
 
 const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 //import routers
 const adminRouter = require(path.join(__dirname, 'routes', 'admin.js'));
 const usersRouter = require(path.join(__dirname, 'routes', 'users.js'));
 
-const app = express();
-
 //config express
 app.set('views', 'views');
 app.set('template engine', 'ejs');
 
-//the middleware
-//  serve public folder
-app.use(express.static(path.join(__dirname, 'public')));
+const mongoUri =
+    'mongodb+srv://main-server:8ryPNpKs1keznyid@nodejs-demo.v9yvf.mongodb.net/authentication?retryWrites=true&w=majority';
 
-//  body parser
-app.use(express.urlencoded({ extended: false }));
+mongoose
+    .connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(3000, console.log('Server is listening'));
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+const store = new MongoDBStore(
+    {
+        uri: mongoUri,
+        collection: 'sessions',
+    },
+    (err) => {
+        if (err) {
+            console.log(err);
+        }
+    }
+);
+
+app.use(
+    session({
+        secret: 'shhhh',
+        resave: false,
+        saveUninitialized: false,
+        store: store, //this prop defaults to new MemoryStore instance
+    })
+);
+
+//the middleware
+app.use(express.static(path.join(__dirname, 'public'))); //serve public
+app.use(express.urlencoded({ extended: false })); //body parser
 
 //  home page
 app.get('/', (req, res, next) => {
@@ -33,5 +65,3 @@ app.use(usersRouter);
 app.use('/', (req, res, next) => {
     res.status(404).render('404.ejs');
 });
-
-app.listen(3000);
