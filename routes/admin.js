5384377f38cf085a696532956dbdc5a16e6bb43a
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-07-25 00:25:38 +0200

    Add list of users from Mongo users collection

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
-const db = require(path.join(rootDir, 'models', 'db.js'));
+const userModel = require(path.join(rootDir, 'models', 'db.js'));
 
 const router = express.Router();
 
@@ -12,16 +12,28 @@ router.get('/', (req, res, next) => {
     res.render('admin.ejs');
 });
 
-//render list of users fetched from database
-router.get('/users', (req, res, next) => {
-    db.fetchUsers()
-        .then(([rows, fields]) => {
-            res.render('admin-users.ejs', { rows: rows });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.send(err);
-        });
+//render list of users fetched from MongoDB collection
+router.get('/users', async (req, res, next) => {
+    let rows = [];
+    try {
+        const docsList = await userModel.find(
+            {},
+            'username email phone address'
+        );
+        for (let i = 0; i < docsList.length; i++) {
+            //get list of rows from query result
+            let currentDoc = docsList[i];
+            let row = [];
+            row.push(currentDoc.username);
+            row.push(currentDoc.email);
+            row.push(currentDoc.phone || 'null');
+            row.push(currentDoc.address || 'null');
+            rows.push(row);
+        }
+        res.render('admin-users.ejs', { rows: rows });
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 module.exports = router;
