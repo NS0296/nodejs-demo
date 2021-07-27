62461b9fa9d8a4049462118f869215b943885665
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-07-27 20:01:49 +0200

    Add id column to users table view

--- routes/admin.js
+++ routes/admin.js
@@ -16,14 +16,12 @@ router.get("/", (req, res, next) => {
 router.get("/users", async (req, res, next) => {
     let rows = [];
     try {
-        const docsList = await User.findAll({
-            //returns list rows in javascript objects
-            attributes: ["username", "email", "phone", "address"],
-        });
+        const docsList = await User.findAll();
+        //returns list rows in javascript objects
         for (let i = 0; i < docsList.length; i++) {
-            //get list of rows from query result
             let currentDoc = docsList[i];
             let row = [];
+            row.push(currentDoc.id);
             row.push(currentDoc.username);
             row.push(currentDoc.email);
             row.push(currentDoc.phone || "null");
