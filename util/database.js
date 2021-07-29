fd6f6f35b8e25b81d3042dc2d4cece2fccfdaa95
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-07-29 18:28:29 +0200

    Disable logging in sequelize

--- util/database.js
+++ util/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("node-demo", "root", "***REMOVED***", {
     host: "localhost",
     dialect: "mysql",
+    logging: false,
 });
 
 module.exports = sequelize;
