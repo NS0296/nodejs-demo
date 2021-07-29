820deb931793fc66b8a3f48c05046e32b65ec1f3
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-07-29 17:16:17 +0200

    Transfer router functions froms routes to controllers for users

---
+++ controllers/users.js
@@ -0,0 +1,56 @@
+const User = require("../models/users.js");
+const bcrypt = require("bcryptjs");
+
+exports.getRegister = (req, res, next) => {
+    res.render("user-register.ejs");
+};
+
+exports.postRegister = async (req, res, next) => {
+    const { username, email, password, phone, address } = req.body;
+    try {
+        const isUserExist = await User.findOne({ where: { email: email } });
+        if (isUserExist !== null) {
+            throw new Error("User already exists");
+        }
+        const hashPassword = await bcrypt.hash(password, 4);
+        User.create({
+            username: username,
+            email: email,
+            password: hashPassword,
+            phone: phone || null,
+            address: address || null,
+        });
+    } catch (err) {
+        console.log(err);
+    }
+    res.redirect("/");
+};
+
+exports.getLogin = (req, res, next) => {
+    res.render("user-login.ejs");
+};
+
+exports.postLogin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ where: { email: email } });
+        if (user === null) {
+            throw new Error("User does not exist");
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            throw new Error("Wrong Password");
+        }
+        req.session.isAuth = true;
+        req.session.userId = user.get("id"); //used to view data in user dashboard
+    } catch (err) {
+        console.log(err);
+    }
+
+    res.redirect("/");
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy();
+    res.redirect("/");
+};
--- routes/users.js
+++ routes/users.js
@@ -1,70 +1,18 @@
 const path = require("path");
 
 const express = require("express");
-const bcrypt = require("bcryptjs");
-
-const rootDir = require("../util/path");
-
-const User = require(path.join(rootDir, "models", "users.js"));
-// const store = require(path.join(rootDir, "app.js"));
-
 const router = express.Router();
 
-//register
-router.get("/register", (req, res, next) => {
-    res.render("user-register.ejs");
-});
+const usersController = require("../controllers/users.js");
 
-router.post("/register", async (req, res, next) => {
-    const { username, email, password, phone, address } = req.body;
-    try {
-        const isUserExist = await User.findOne({ where: { email: email } });
-        if (isUserExist !== null) {
-            throw new Error("User already exists");
-        }
-        const hashPassword = await bcrypt.hash(password, 4);
-        User.create({
-            username: username,
-            email: email,
-            password: hashPassword,
-            phone: phone || null,
-            address: address || null,
-        });
-    } catch (err) {
-        console.log(err);
-    }
-    res.redirect("/");
-});
+router.get("/register", usersController.getRegister);
 
-//login
-router.get("/login", (req, res, next) => {
-    res.render("user-login.ejs");
-});
+router.post("/register", usersController.postRegister);
 
-router.post("/login", async (req, res, next) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ where: { email: email } });
-        if (user === null) {
-            throw new Error("User does not exist");
-        }
-        const isPasswordMatch = await bcrypt.compare(password, user.password);
-        if (!isPasswordMatch) {
-            throw new Error("Wrong Password");
-        }
-        req.session.isAuth = true;
-        req.session.userId = user.get("id"); //used to view data in user dashboard
-    } catch (err) {
-        console.log(err);
-    }
+router.get("/login", usersController.getLogin);
 
-    res.redirect("/");
-});
+router.post("/login", usersController.postLogin);
 
-//logout
-router.post("/logout", (req, res, next) => {
-    req.session.destroy();
-    res.redirect("/");
-});
+router.post("/logout", usersController.postLogout);
 
 module.exports = router;
