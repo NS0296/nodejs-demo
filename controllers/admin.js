b1d5d13a84b54136fcebfba414744b3e46e8a7d6
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-07-29 18:43:55 +0200

    Transfer functions in admin router to admin controller

---
+++ controllers/admin.js
@@ -0,0 +1,62 @@
+const User = require("../models/users.js");
+
+exports.getAdmin = (req, res, next) => {
+    res.render("admin.ejs");
+};
+
+exports.getUsersTable = async (req, res, next) => {
+    let rows = [];
+    try {
+        const docsList = await User.findAll();
+        //returns list rows in javascript objects
+        for (let i = 0; i < docsList.length; i++) {
+            let currentDoc = docsList[i];
+            let row = [];
+            row.push(currentDoc.id);
+            row.push(currentDoc.username);
+            row.push(currentDoc.email);
+            row.push(currentDoc.phone || "null");
+            row.push(currentDoc.address || "null");
+            rows.push(row);
+        }
+        res.render("admin-users.ejs", { rows: rows });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+exports.postDeleteUser = async (req, res, next) => {
+    const userId = parseInt(req.params.userId);
+    console.log(typeof userId, userId);
+    const deleteRow = await User.destroy({ where: { id: userId } });
+    res.redirect("/admin/users");
+};
+
+exports.postEditUser = async (req, res, next) => {
+    const userId = parseInt(req.params.userId); //sourced from users table view dynamic route
+    const { username, email, phone, address } = await User.findByPk(userId);
+    res.render("update-row.ejs", {
+        userId: userId,
+        username: username,
+        email: email,
+        phone: phone,
+        address: address,
+    });
+};
+
+exports.postEditUserConfirm = async (req, res, next) => {
+    const userId = parseInt(req.params.userId); //sourced from edit form (above)
+    const { username, email, phone, address } = req.body;
+    const updateRow = await User.update(
+        {
+            username: username,
+            email: email,
+            phone: phone,
+            address: address,
+        },
+        {
+            where: { id: userId },
+        }
+    );
+    res.redirect("/admin/users");
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,74 +1,17 @@
 //responsibe for all admin routes
-const path = require("path");
-
 const express = require("express");
-
-const rootDir = require("../util/path");
-const { type } = require("os");
-const User = require(path.join(rootDir, "models", "users.js"));
-
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    res.render("admin.ejs");
-});
+const adminController = require("../controllers/admin.js");
 
-//render list of users fetched from MongoDB collection
-router.get("/users", async (req, res, next) => {
-    let rows = [];
-    try {
-        const docsList = await User.findAll();
-        //returns list rows in javascript objects
-        for (let i = 0; i < docsList.length; i++) {
-            let currentDoc = docsList[i];
-            let row = [];
-            row.push(currentDoc.id);
-            row.push(currentDoc.username);
-            row.push(currentDoc.email);
-            row.push(currentDoc.phone || "null");
-            row.push(currentDoc.address || "null");
-            rows.push(row);
-        }
-        res.render("admin-users.ejs", { rows: rows });
-    } catch (err) {
-        console.log(err);
-    }
-});
+router.get("/", adminController.getAdmin);
 
-router.post("/users/delete/:userId", async (req, res, next) => {
-    const userId = parseInt(req.params.userId);
-    console.log(typeof userId, userId);
-    const deleteRow = await User.destroy({ where: { id: userId } });
-    res.redirect("/admin/users");
-});
+router.get("/users", adminController.getUsersTable);
 
-router.post("/users/edit/:userId", async (req, res, next) => {
-    const userId = parseInt(req.params.userId); //sourced from users table view dynamic route
-    const { username, email, phone, address } = await User.findByPk(userId);
-    res.render("update-row.ejs", {
-        userId: userId,
-        username: username,
-        email: email,
-        phone: phone,
-        address: address,
-    });
-});
+router.post("/users/delete/:userId", adminController.postDeleteUser);
 
-router.post("/users/edit/confirm/:userId", async (req, res, next) => {
-    const userId = parseInt(req.params.userId); //sourced from edit form
-    const { username, email, phone, address } = req.body;
-    const updateRow = await User.update(
-        {
-            username: username,
-            email: email,
-            phone: phone,
-            address: address,
-        },
-        {
-            where: { id: userId },
-        }
-    );
-    res.redirect("/admin/users");
-});
+router.post("/users/edit/:userId", adminController.postEditUser);
+
+router.post("/users/edit/confirm/:userId", adminController.postEditUserConfirm);
 
 module.exports = router;
