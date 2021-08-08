263dfe0c4ea94aa2cc48e30c867ebc8ff2734f66
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-08 22:07:55 +0200

    Add order items junction table

--- util/database.js
+++ util/database.js
@@ -11,3 +11,17 @@ module.exports = sequelize;
 const User = require("../models/user");
 const Item = require("../models/item");
 const Order = require("../models/order");
+const OrderItem = require("../models/order-item");
+
+//initialize relationships
+User.hasMany(Order, {
+    onDelete: "RESTRICT",
+    onUpdate: "NO ACTION",
+    foreignKey: {
+        allowNull: false,
+    },
+});
+Order.belongsTo(User);
+
+Order.belongsToMany(Item, { through: OrderItem });
+Item.belongsToMany(Order, { through: OrderItem });
