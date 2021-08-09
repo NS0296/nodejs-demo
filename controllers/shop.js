52b2fb2e2f07423155e801527e2d6d450ab1276c
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-10 01:51:14 +0200

    Rename shop.ejs, remove columns from table

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,7 +4,7 @@ exports.getAllShop = async (req, res, next) => {
     try {
         const fetchRes = await fetch("http://localhost:3000/api/items/all");
         const allItems = await fetchRes.json();
-        res.render("shop/shop.ejs", {
+        res.render("shop/index.ejs", {
             isAuth: req.session.isAuth,
             allItems: allItems,
             pageTitle: "Home",
