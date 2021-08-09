c445dcdb8b0950d51b5fcf6818e9d5e1d6be7ba0
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-09 23:57:53 +0200

    Render items rows in shop.ejs

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,16 @@
-exports.getAllShop = (req, res, next) => {
-    res.render("shop.ejs", {
-        isAuth: req.session.isAuth,
-        pageTitle: "Home",
-        path: "/",
-    });
+const fetch = require("node-fetch");
+
+exports.getAllShop = async (req, res, next) => {
+    try {
+        const fetchRes = await fetch("http://localhost:3000/api/items/all");
+        const allItems = await fetchRes.json();
+        res.render("shop/shop.ejs", {
+            isAuth: req.session.isAuth,
+            allItems: allItems,
+            pageTitle: "Home",
+            path: "/",
+        });
+    } catch (err) {
+        console.log(err);
+    }
 };
