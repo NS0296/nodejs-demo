2e8c0e2c9f9171a010658eedac04396979e3b817
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-10 23:42:28 +0200

    Add Post order api route

--- controllers/api/orders.js
+++ controllers/api/orders.js
@@ -1,11 +1,26 @@
 const Order = require("../../models/order");
 const User = require("../../models/user");
+const fetch = require("node-fetch");
 
 exports.postOrder = async (req, res) => {
     try {
-        const user = await User.findOne({ where: { id: req.params.userId } });
+        const { paymentMethod, shippingAddress } = req.body;
+        const userId = req.params.userId;
+        console.log(userId);
+        const fetchRes = await fetch(`http://localhost:3000/api/carts/summary/${userId}`);
+        const cartSummary = await fetchRes.json();
+        const user = await User.findOne({ where: { id: userId } });
         const userCart = await user.getCart();
-        res.send({ test: userCart });
+        const userCartItems = await userCart.getItems();
+        const newOrder = await user.createOrder({
+            date: Date.now(),
+            paymentMethod: paymentMethod,
+            totalPrice: cartSummary.totalPrice,
+            shippingAddress: shippingAddress,
+        });
+        newOrder.setItems(userCartItems);
+        userCart.setItems(null);
+        res.send(newOrder);
     } catch (err) {
         res.send(err);
     }
