0cb419acdeecded40892d252687ecf0372de7f82
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-10 16:35:20 +0200

    Make index dependant on authentication status

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,13 +1,21 @@
 const fetch = require("node-fetch");
 
-exports.getAllShop = async (req, res, next) => {
+exports.getWelcome = (req, res) => {
+    res.render("welcome.ejs", {
+        isAuth: req.session.isAuth,
+        pageTitle: "Home",
+        path: "/",
+    });
+};
+
+exports.getIndex = async (req, res, next) => {
     try {
         const fetchRes = await fetch("http://localhost:3000/api/items/all");
         const allItems = await fetchRes.json();
         res.render("shop/index.ejs", {
             isAuth: req.session.isAuth,
             allItems: allItems,
-            pageTitle: "Home",
+            pageTitle: "Shop",
             path: "/",
         });
     } catch (err) {
