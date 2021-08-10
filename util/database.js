5ab7c5d9a2d812a2e0b5b487a68ccc7bcded7d6f
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-10 02:39:58 +0200

    Add cart model and cart-item junction model

--- util/database.js
+++ util/database.js
@@ -10,6 +10,8 @@ module.exports = sequelize;
 
 const User = require("../models/user");
 const Item = require("../models/item");
+const Cart = require("../models/cart");
+const CartItem = require("../models/cart-item");
 const Order = require("../models/order");
 const OrderItem = require("../models/order-item");
 
@@ -22,6 +24,18 @@ User.hasMany(Order, {
     },
 });
 Order.belongsTo(User);
+
+User.hasOne(Cart, {
+    onDelete: "NO ACTION",
+    onUpdate: "NO ACTION",
+    foreignKey: {
+        allowNull: false,
+    },
+});
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Item, { through: CartItem });
+Item.belongsToMany(Cart, { through: CartItem });
 
 Order.belongsToMany(Item, { through: OrderItem });
 Item.belongsToMany(Order, { through: OrderItem });
