97fbfa220e63dbf6acfe6b4432be2e3b82f08dff
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-10 16:35:48 +0200

    Belongs to previoes commit

--- routes/shop.js
+++ routes/shop.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 
 const shopController = require("../controllers/shop.js");
 
-router.get("/", shopController.getAllShop);
+router.get("/", shopController.getIndex);
 
 module.exports = router;
