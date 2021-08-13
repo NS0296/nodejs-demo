093292aa79103449f4193f2e2171a047b7c8e251
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-13 17:45:39 +0200

    Convert update user route to put instead of post

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,10 +4,10 @@ const apiController = require("../../controllers/api/users.js");
 
 router.get("/all", apiController.allUsers);
 
-router.delete("/delete/:userId", apiController.deleteUser);
-
-router.post("/update/:userId", apiController.updateUser);
-
 router.post("/insert", apiController.insert);
+
+router.put("/update/:userId", apiController.updateUser);
+
+router.delete("/delete/:userId", apiController.deleteUser);
 
 module.exports = router;
