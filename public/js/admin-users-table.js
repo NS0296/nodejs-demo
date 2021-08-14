0f8b0550dcf975c0bba15e3922711a20b2495eb0
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-14 23:39:01 +0200

    Make admin users table work again using new models

--- public/js/admin-users-table.js
+++ public/js/admin-users-table.js
@@ -28,147 +28,92 @@ let getAllSiblings = e => {
     return siblings;
 };
 
-class UserData {
-    constructor(username, email, phone, address) {
-        this.username = username;
-        this.email = email;
-        this.phone = phone;
-        this.address = address;
-    }
-}
+const addActionCells = () => {
+    for (let i = 0; i < tblBody.children.length; i++) {
+        const user = tblBody.children[i];
 
-const setupTable = () => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://localhost:3000/api/users/all", true);
+        //create action cell
+        let cellActions = document.createElement("td");
+        cellActions.className = "actions";
 
-    xhr.onload = () => {
-        if (xhr.status === 200) {
-            const res = JSON.parse(xhr.responseText);
-            //creating all cells
-            res.forEach(user => {
-                let row = document.createElement("tr");
+        //create delete action button
+        let actionButtonDelete = document.createElement("button");
+        actionButtonDelete.className = "actionButtons deleteButtons";
+        actionButtonDelete.innerText = "Delete";
 
-                //creates a cell per column property
-                let cellId = document.createElement("td");
-                let cellUsername = document.createElement("td");
-                let cellEmail = document.createElement("td");
-                let cellPhone = document.createElement("td");
-                let cellAddress = document.createElement("td");
+        actionButtonDelete.addEventListener("click", () => {
+            const xhr = new XMLHttpRequest();
+            let reqUrl = `http://localhost:3000/api/users/destroy/${user.dataset.userId}`;
+            xhr.open("DELETE", reqUrl, true);
 
-                //add classes to all cells by column
-                cellId.className = "id userData";
-                cellUsername.className = "userData username";
-                cellEmail.className = "userData email";
-                cellPhone.className = "userData phone";
-                cellAddress.className = "userData address";
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    getNthParentOf(actionButtonDelete, 2).remove(); /*use user var*/
+                }
+            };
 
-                //add text to each cell
-                cellId.innerText = user.id;
-                cellUsername.innerText = user.username;
-                cellEmail.innerText = user.email;
-                cellPhone.innerText = user.phone;
-                cellAddress.innerText = user.address;
+            xhr.send();
+        });
 
-                row.appendChild(cellId);
-                row.appendChild(cellUsername);
-                row.appendChild(cellEmail);
-                row.appendChild(cellPhone);
-                row.appendChild(cellAddress);
+        //create edit action button
+        let actionButtonEdit = document.createElement("button");
+        actionButtonEdit.className = "actionButton editButton";
+        actionButtonEdit.innerText = "Edit";
 
-                //add actions cell
-                let cellActions = document.createElement("td");
-                cellActions.className = "actions";
-
-                //delete action button
-                let actionButtonDelete = document.createElement("button");
-                actionButtonDelete.className = "actionButtons deleteButtons";
-                actionButtonDelete.innerText = "Delete";
-                actionButtonDelete.dataset.userId = user.id;
-
-                actionButtonDelete.addEventListener("click", () => {
-                    const xhr = new XMLHttpRequest();
-                    let reqUrl = `http://localhost:3000/api/users/delete/${actionButtonDelete.dataset.userId}`;
-                    xhr.open("DELETE", reqUrl, true);
-
-                    xhr.onload = () => {
-                        if (xhr.status === 200) {
-                            let deletedRow = getNthParentOf(actionButtonDelete, 2);
-                            deletedRow.remove();
-                        }
-                    };
-                    xhr.send();
-                });
-
-                //edit action button
-                let actionButtonEdit = document.createElement("button");
-                actionButtonEdit.className = "actionButton editButton";
-                actionButtonEdit.innerText = "Edit";
-                actionButtonEdit.dataset.userId = user.id;
-                actionButtonEdit.addEventListener("click", () => {
-                    const allCells = getAllSiblings(actionButtonEdit.parentNode);
-                    allCells.shift();
-                    cellActions.replaceChild(actionButtonUpdate, actionButtonEdit);
-                    allCells.forEach(cell => {
-                        const input = document.createElement("input");
-                        const newCell = document.createElement("td");
-                        input.value = cell.innerText;
-                        newCell.appendChild(input);
-                        cell.parentNode.replaceChild(
-                            newCell,
-                            cell.parentNode.childNodes[allCells.indexOf(cell) + 1]
-                        );
-                    });
-                });
-
-                //update action button
-                let actionButtonUpdate = document.createElement("button");
-                actionButtonUpdate.className = "actionButton updateButton";
-                actionButtonUpdate.innerText = "Update";
-                actionButtonUpdate.dataset.userId = user.id;
-
-                actionButtonUpdate.addEventListener("click", () => {
-                    const rowCells = getAllSiblings(actionButtonUpdate.parentNode);
-                    rowCells.shift(); //remove id cell
-                    cellActions.replaceChild(actionButtonEdit, actionButtonUpdate);
-                    const newData = new UserData(
-                        rowCells[0].firstChild.value,
-                        rowCells[1].firstChild.value,
-                        rowCells[2].firstChild.value,
-                        rowCells[3].firstChild.value
-                    );
-
-                    for (let i = 0; i < rowCells.length; i++) {
-                        const cell = rowCells[i];
-                        const textNode = document.createTextNode(cell.firstChild.value); //firstchild -> input
-                        cell.replaceChild(textNode, cell.firstChild);
-                    }
-
-                    const xhr = new XMLHttpRequest();
-                    let reqUrl = `http://localhost:3000/api/users/update/${actionButtonUpdate.dataset.userId}`;
-                    xhr.open("POST", reqUrl, true);
-
-                    xhr.onload = () => {
-                        if (xhr.status === 200) {
-                            console.log(xhr.response);
-                        }
-                    };
-
-                    xhr.setRequestHeader(
-                        "Content-Type",
-                        "application/json;charset=UTF-8"
-                    );
-                    xhr.setRequestHeader("Accept", "application/json");
-                    xhr.send(JSON.stringify(newData));
-                });
-
-                cellActions.appendChild(actionButtonDelete);
-                cellActions.appendChild(actionButtonEdit);
-                row.appendChild(cellActions);
-
-                tblBody.appendChild(row);
+        actionButtonEdit.addEventListener("click", () => {
+            const allRowCells = getAllSiblings(actionButtonEdit.parentNode);
+            allRowCells.shift();
+            cellActions.replaceChild(actionButtonUpdate, actionButtonEdit);
+            allRowCells.forEach(cell => {
+                const input = document.createElement("input");
+                input.value = cell.innerText;
+                cell.replaceChild(input, cell.firstChild);
             });
-        }
-    };
-    xhr.send();
+        });
+
+        //create update action button
+        let actionButtonUpdate = document.createElement("button");
+        actionButtonUpdate.className = "actionButton updateButton";
+        actionButtonUpdate.innerText = "Update";
+
+        actionButtonUpdate.addEventListener("click", () => {
+            const allRowCells = getAllSiblings(actionButtonUpdate.parentNode);
+            allRowCells.shift();
+            cellActions.replaceChild(actionButtonEdit, actionButtonUpdate);
+            const newUserDate = {
+                username: allRowCells[0].firstChild.value,
+                email: allRowCells[1].firstChild.value,
+                phone: allRowCells[2].firstChild.value,
+                home_address: allRowCells[3].firstChild.value,
+            };
+
+            //send new data in ajax call
+            const xhr = new XMLHttpRequest();
+            let reqUrl = `http://localhost:3000/api/users/update/${user.dataset.userId}`;
+            xhr.open("PUT", reqUrl, true);
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    if (JSON.parse(xhr.response)[0].affectedRows >= 1) {
+                        for (let i = 0; i < allRowCells.length; i++) {
+                            const cell = allRowCells[i];
+                            const textNode = document.createTextNode(
+                                cell.firstChild.value
+                            );
+                            cell.replaceChild(textNode, cell.firstChild);
+                        }
+                    }
+                }
+            };
+
+            xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            xhr.setRequestHeader("Accept", "application/json");
+            xhr.send(JSON.stringify(newUserDate));
+        });
+
+        cellActions.appendChild(actionButtonDelete);
+        cellActions.appendChild(actionButtonEdit);
+        user.appendChild(cellActions);
+    }
 };
-document.addEventListener("DOMContentLoaded", setupTable);
+
+document.addEventListener("DOMContentLoaded", addActionCells);
