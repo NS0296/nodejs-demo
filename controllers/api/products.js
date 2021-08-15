a610a0f4d54f3d7b6352efd9303b06ec4d8db42a
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-15 18:46:33 +0200

    Adapt rest of api funcs to new model

--- controllers/api/products.js
+++ controllers/api/products.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const Product = require("../../models/product");
 
 exports.findAll = (req, res, next) => {
@@ -8,64 +9,25 @@ exports.findAll = (req, res, next) => {
         .catch(err => res.send(err));
 };
 
-exports.deleteItem = async (req, res, next) => {
-    try {
-        // const itemId = parseInt(req.params.itemId);
-        // const deleteRow = await Item.destroy({ where: { id: itemId } });
-        res.send({ status: "done" });
-    } catch (err) {
-        res.send(err);
-    }
+exports.destroy = (req, res, next) => {
+    const productId = req.params.productId;
+    Product.deleteByPK(productId)
+        .then(response => res.send(response))
+        .catch(err => res.send(err));
 };
 
-exports.createItem = async (req, res, next) => {
-    const { name, categoryName, manufacture, price, stockAvailable } = req.body;
-    try {
-        // const createItem = await Item.create({
-        //     name: name,
-        //     categoryName: categoryName,
-        //     manufacture: manufacture,
-        //     price: price,
-        //     stockAvailable,
-        //     dateFirstAvailable: Date.now(),
-        // });
-        res.send({ message: "Created Item" });
-    } catch (err) {
-        res.send(err);
-    }
+exports.create = (req, res, next) => {
+    const { title, categoryName, price, stock } = req.body;
+    const product = new Product(title, categoryName, price, stock);
+    product
+        .save()
+        .then(response => res.send(response))
+        .catch(err => res.send(err));
 };
 
-exports.updateItem = async (req, res, next) => {
-    const {
-        id,
-        name,
-        categoryName,
-        manufacture,
-        price,
-        stockAvailable,
-        dateFirstAvailable,
-    } = req.body;
-    try {
-        const itemId = parseInt(req.params.itemId);
-        // const updateItem = await Item.update(
-        //     {
-        //         id: id,
-        //         name: name,
-        //         categoryName: categoryName,
-        //         manufacture: manufacture,
-        //         price: price,
-        //         stockAvailable: stockAvailable,
-        //         dateFirstAvailable: dateFirstAvailable,
-        //     },
-        //     {
-        //         where: { id: itemId },
-        //     }
-        // );
-        if (updateItem[0] === 0) {
-            throw new Error("Item does not exit");
-        }
-        res.send({ status: "done" });
-    } catch (err) {
-        res.send(err);
-    }
+exports.update = (req, res, next) => {
+    const productId = req.params.itemId;
+    Product.updateByPK(productId, req.body)
+        .then(response => res.send(response))
+        .catch(err => res.send(err));
 };
