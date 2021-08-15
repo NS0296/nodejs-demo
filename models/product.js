b9e8362614000438ac5271060c2e2df8a0708f4c
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-15 17:28:32 +0200

    Add findAll static method

--- models/product.js
+++ models/product.js
@@ -1,3 +1,33 @@
+const pool = require("../util/database").promisePool;
+
+class Product {
+    constructor(title, categoryName, price, stock) {
+        this.title = title;
+        this.categoryName = categoryName;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    static pool = pool;
+
+    get pool() {
+        return User.pool;
+    }
+
+    static findAll(filters) {
+        if (filters === undefined) filters = {}; //prevent error incase no arg. is passed
+        return this.pool.execute("CALL get_all_users(?, ?, ?, ?, ?);", [
+            filters.id || null,
+            filters.title || null,
+            filters.categoryName || null,
+            filters.price || null,
+            filters.stock || null,
+        ]);
+    }
+}
+
+module.exports = Product;
+
 // const { Sequelize } = require("sequelize"); //import sequlize for datatypes
 // const sequelize = require("../util/database"); //import connection object
 
