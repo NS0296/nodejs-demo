dcfc62136192dce89f2888786c1718ce7c1d6f46
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-20 20:07:01 +0200

    Quick fix

--- controllers/api/orders.js
+++ controllers/api/orders.js
@@ -3,12 +3,12 @@ const Order = require("../../models/order");
 exports.postCreateOrder = async (req, res) => {
     const { payment_method, shipping_address } = req.body;
     try {
-        Order.createOrder({
+        await Order.createOrder({
             userId: req.session.id,
             paymentMethod: 1,
             shippingAddress: shipping_address,
         });
-        res.send(newOrder);
+        res.send({ message: "success" });
     } catch (err) {
         res.send(err);
     }
