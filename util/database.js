7760800796efdc42b2d81768582bed11defe028b
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-22 01:53:25 +0200

    Change database for session store

--- util/database.js
+++ util/database.js
@@ -1,12 +1,16 @@
-//this block is not commented out for now because express session uses sequelize
-const Sequelize = require("sequelize");
+const Sequelize = require("sequelize"); //sequelize is used for session-store
 const mysql = require("mysql2");
 
-const sequelize = new Sequelize("node-demo", "root", process.env.MYSQLPASSWORD, {
-    host: "localhost",
-    dialect: "mysql",
-    logging: false,
-});
+const sequelize = new Sequelize(
+    process.env.MYSQLDEFAULTDATABASE,
+    "root",
+    process.env.MYSQLPASSWORD,
+    {
+        host: "localhost",
+        dialect: "mysql",
+        logging: false,
+    }
+);
 
 const pool = mysql.createPool({
     host: "localhost",
@@ -20,35 +24,3 @@ const pool = mysql.createPool({
 
 exports.promisePool = pool.promise();
 exports.sequelize = sequelize;
-
-// const User = require("../models/user");
-// const Item = require("../models/item");
-// const Cart = require("../models/cart");
-// const CartItem = require("../models/cart-item");
-// const Order = require("../models/order");
-// const OrderItem = require("../models/order-item");
-
-// //initialize relationships
-// User.hasMany(Order, {
-//     onDelete: "RESTRICT",
-//     onUpdate: "NO ACTION",
-//     foreignKey: {
-//         allowNull: false,
-//     },
-// });
-// Order.belongsTo(User);
-
-// User.hasOne(Cart, {
-//     onDelete: "NO ACTION",
-//     onUpdate: "NO ACTION",
-//     foreignKey: {
-//         allowNull: false,
-//     },
-// });
-// Cart.belongsTo(User);
-
-// Cart.belongsToMany(Item, { through: CartItem });
-// Item.belongsToMany(Cart, { through: CartItem });
-
-// Order.belongsToMany(Item, { through: OrderItem });
-// Item.belongsToMany(Order, { through: OrderItem });
