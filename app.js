fab4a7925e6eb2383ac51cc7bd5fa6e98eb539ff
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-21 19:56:33 +0200

    Refactor

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
+require("dotenv").config();
 const path = require("path");
-const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
-//const sequelize = require("./util/database");
 const session = require("express-session");
-const store = require("./util/session");
+const store = require("./util/session"); //session store
+
+const shop = require(path.join(__dirname, "routes", "shop.js"));
+const auth = require(path.join(__dirname, "routes", "auth.js"));
+const admin = require(path.join(__dirname, "routes", "admin.js"));
+const errorController = require("./controllers/error.js");
+const { userApi, productApi, cartApi, orderApi } = require(path.join(
+    __dirname,
+    "routes",
+    "api",
+    "index.js"
+));
 
 //config express
 app.set("views", "views");
 app.set("template engine", "ejs");
 
+//run server
+(() => {
+    try {
+        app.listen(3000);
+        console.log("app is listening on http://localhost:3000/");
+    } catch (err) {
+        console.log(err);
+    }
+})();
+
+//the middleware
 app.use(
     session({
         secret: "shhhh",
@@ -18,40 +39,15 @@ app.use(
         store: store,
     })
 );
-
-(async () => {
-    try {
-        // await sequelize.authenticate();
-        // await sequelize.sync();
-        app.listen(3000, console.log("app is listening on http://localhost:3000/"));
-    } catch (err) {
-        console.log(err);
-    }
-})();
-
-const errorController = require("./controllers/error.js");
-
-const admin = require(path.join(__dirname, "routes", "admin.js"));
-const auth = require(path.join(__dirname, "routes", "auth.js"));
-const shop = require(path.join(__dirname, "routes", "shop.js"));
-const { usersApi, productsApi, cartsApi, ordersApi } = require(path.join(
-    __dirname,
-    "routes",
-    "api",
-    "index.js"
-));
-
-//the middleware
 app.use(express.static(path.join(__dirname, "public"))); //serve public
-app.use(express.urlencoded({ extended: false })); //body parser for urlencoded
-app.use(express.json()); //body parser for json
+app.use(express.urlencoded({ extended: false })); //parser for urlencoded data
+app.use(express.json()); //parser for json
 
-//  use routers
 app.use(shop);
 app.use(auth);
-app.use("/api/users", usersApi);
-app.use("/api/products", productsApi);
-app.use("/api/carts", cartsApi);
-app.use("/api/orders", ordersApi);
+app.use("/api/user", usersApi);
+app.use("/api/product", productsApi);
+app.use("/api/cart", cartsApi);
+app.use("/api/order", ordersApi);
 app.use("/admin", admin);
 app.use(errorController);
