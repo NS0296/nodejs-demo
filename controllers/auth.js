d4456361347484eb24070a1a8b0168d58b65194b
Waleyeldeen <waleyeldeenahmed@gmail.com>
2021-08-22 15:31:01 +0200

    Clean up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,22 @@
-const User = require("../models/user.js");
-const rootDir = require("../util/path.js");
-const path = require("path");
 const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
 const ejs = require("ejs");
 const Sequelize = require("sequelize");
 const transporter = require("../util/sendgrid.js");
+const path = require("path");
+const User = require("../models/user.js");
+const rootDir = require("../util/path.js");
 
-exports.getRegister = (req, res, next) => {
+exports.getRegister = (req, res) => {
     res.render("auth/user-register.ejs", {
         pageTitle: "Register",
-        isAuth: req.session.isAuth,
         path: "/register",
+        isAuth: req.session.isAuth,
     });
 };
 
-exports.postRegister = async (req, res, next) => {
+exports.postRegister = async (req, res) => {
     const { username, email, password, phone, home_address } = req.body;
-    res.redirect("/login");
     try {
         const [isEmailExist] = await User.findAll({ email: email });
         if (isEmailExist[0].length) {
@@ -36,22 +35,24 @@ exports.postRegister = async (req, res, next) => {
         const hashPassword = await bcrypt.hash(password, 4);
         const user = new User(username, email, hashPassword, phone, home_address);
         await user.save();
+        res.redirect("/login");
     } catch (err) {
         console.log(err);
     }
 };
 
-exports.getLogin = (req, res, next) => {
+exports.getLogin = (req, res) => {
     res.render("auth/user-login.ejs", {
         pageTitle: "Login",
-        isAuth: req.session.isAuth,
         path: "/login",
+        isAuth: req.session.isAuth,
     });
 };
 
-exports.postLogin = async (req, res, next) => {
+exports.postLogin = async (req, res) => {
     const { email, password } = req.body;
     try {
+        //task: create a sql function to check for existing user
         const [[user]] = await User.findAll({ email: email });
         if (!user.length) {
             throw new Error("User does not exist");
@@ -68,129 +69,130 @@ exports.postLogin = async (req, res, next) => {
     res.redirect("/");
 };
 
-exports.getReset = (req, res, next) => {
-    res.render("auth/reset.ejs", {
-        pageTitle: "Reset Password",
-        path: "/reset",
-        isAuth: req.session.isAuth,
-    });
-};
+//task: adapt password reset logic to using database engine function and procedures
+// exports.getReset = (req, res) => {
+//     res.render("auth/reset.ejs", {
+//         pageTitle: "Reset Password",
+//         path: "/reset",
+//         isAuth: req.session.isAuth,
+//     });
+// };
 
-exports.postReset = async (req, res, next) => {
-    const { email } = req.body;
-    res.redirect("/login");
-    try {
-        const token = await crypto.randomBytes(15).toString("hex");
-        const user = await User.findOne({ where: { email: email } });
-        const updateUser = await User.update(
-            {
-                resetToken: token,
-                resetTokenExpiration: Date.now() + 36000000,
-            },
-            {
-                where: { email: email },
-            }
-        );
-        if (updateUser[0] === 0) {
-            throw new Error("User does not exist");
-        }
-        const mailHtml = await ejs.renderFile(
-            path.join(rootDir, "views", "email", "reset-password.ejs"),
-            {
-                username: user.username,
-                email: email,
-                resetToken: token,
-            }
-        );
-        const mail = await transporter.sendMail({
-            from: process.env.SENDGRID_SENDER_MAIN,
-            to: email,
-            subject: "Reset password",
-            html: mailHtml,
-        });
-    } catch (error) {
-        console.log(error);
-    }
-};
+// exports.postReset = async (req, res) => {
+//     const { email } = req.body;
+//     try {
+//         const token = await crypto.randomBytes(15).toString("hex");
+//         const user = await User.findOne({ where: { email: email } });
+//         const updateUser = await User.update(
+//             {
+//                 resetToken: token,
+//                 resetTokenExpiration: Date.now() + 36000000,
+//             },
+//             {
+//                 where: { email: email },
+//             }
+//         );
+//         if (updateUser[0] === 0) {
+//             throw new Error("User does not exist");
+//         }
+//         const mailHtml = await ejs.renderFile(
+//             path.join(rootDir, "views", "email", "reset-password.ejs"),
+//             {
+//                 username: user.username,
+//                 email: email,
+//                 resetToken: token,
+//             }
+//         );
+//         const mail = await transporter.sendMail({
+//             from: process.env.SENDGRID_SENDER_MAIN,
+//             to: email,
+//             subject: "Reset password",
+//             html: mailHtml,
+//         });
+//         res.redirect("/login");
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
 
-exports.getNewPassword = async (req, res, next) => {
-    const resetToken = req.params.resetToken;
-    let user;
-    try {
-        user = await User.findOne({
-            where: {
-                resetToken: resetToken,
-                resetTokenExpiration: { [Sequelize.Op.gt]: Date.now() },
-            },
-        });
-        if (user === null) {
-            throw new Error("Reset Token is invalid");
-        }
-    } catch (err) {
-        console.log(err);
-    }
-    res.render("auth/new-password.ejs", {
-        userId: user.id,
-        resetToken: resetToken,
-        pageTitle: "New Password",
-        path: "/new-password",
-        isAuth: req.session.isAuth,
-    });
-};
+// exports.getNewPassword = async (req, res) => {
+//     const resetToken = req.params.resetToken;
+//     let user;
+//     try {
+//         user = await User.findOne({
+//             where: {
+//                 resetToken: resetToken,
+//                 resetTokenExpiration: { [Sequelize.Op.gt]: Date.now() },
+//             },
+//         });
+//         if (user === null) {
+//             throw new Error("Reset Token is invalid");
+//         }
+//     } catch (err) {
+//         console.log(err);
+//     }
+//     res.render("auth/new-password.ejs", {
+//         userId: user.id,
+//         resetToken: resetToken,
+//         pageTitle: "New Password",
+//         path: "/new-password",
+//         isAuth: req.session.isAuth,
+//     });
+// };
 
-exports.postNewPassword = async (req, res, next) => {
-    const { userId, resetToken, password } = req.body;
-    try {
-        const user = await User.findOne({
-            where: {
-                id: userId,
-                resetToken: resetToken,
-                resetTokenExpiration: { [Sequelize.Op.gt]: Date.now() },
-            },
-        });
-        const hashNewPassword = await bcrypt.hash(password, 4);
-        user.update({
-            password: hashNewPassword,
-            resetToken: null,
-            resetTokenExpiration: null,
-        });
-        res.render("auth/user-login.ejs", {
-            pageTitle: "Login",
-            isAuth: req.session.isAuth,
-            path: "/login",
-        });
-        const mailHtml = await ejs.renderFile(
-            path.join(rootDir, "views", "email", "new-password-success.ejs"),
-            {
-                username: user.username,
-            }
-        );
-        const mail = await transporter.sendMail({
-            from: process.env.SENDGRID_SENDER_MAIN,
-            to: user.email,
-            subject: "Reset password succesful",
-            html: mailHtml,
-        });
-    } catch (err) {
-        console.log(err);
-    }
-};
+// exports.postNewPassword = async (req, res, next) => {
+//     const { userId, resetToken, password } = req.body;
+//     try {
+//         const user = await User.findOne({
+//             where: {
+//                 id: userId,
+//                 resetToken: resetToken,
+//                 resetTokenExpiration: { [Sequelize.Op.gt]: Date.now() },
+//             },
+//         });
+//         const hashNewPassword = await bcrypt.hash(password, 4);
+//         user.update({
+//             password: hashNewPassword,
+//             resetToken: null,
+//             resetTokenExpiration: null,
+//         });
+//         res.render("auth/user-login.ejs", {
+//             pageTitle: "Login",
+//             isAuth: req.session.isAuth,
+//             path: "/login",
+//         });
+//         const mailHtml = await ejs.renderFile(
+//             path.join(rootDir, "views", "email", "new-password-success.ejs"),
+//             {
+//                 username: user.username,
+//             }
+//         );
+//         const mail = await transporter.sendMail({
+//             from: process.env.SENDGRID_SENDER_MAIN,
+//             to: user.email,
+//             subject: "Reset password succesful",
+//             html: mailHtml,
+//         });
+//     } catch (err) {
+//         console.log(err);
+//     }
+// };
 
-exports.getUserDashboard = async (req, res, next) => {
+exports.getUserDashboard = async (req, res) => {
     try {
         const [[[user]]] = await User.findAll({ id: req.session.userId });
         res.render("auth/dashboard.ejs", {
-            user: user,
             pageTitle: "Dashboard",
-            isAuth: req.session.isAuth,
             path: "/dashboard",
+            user: user,
+            isAuth: req.session.isAuth,
         });
     } catch (err) {
         console.log(err);
     }
 };
 
-exports.getLogout = (req, res, next) => {
+exports.getLogout = (req, res) => {
     req.session.destroy();
     res.redirect("/");
 };
